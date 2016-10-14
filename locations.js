b987ae273f9e53eb19bf980757cdaea13e015425
heymrhayes <Heymrhayes@gmail.com>
2016-10-14 13:24:52 -0500

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,25 @@
+// locations provide the x,y coordinates for the line drawing
+
+
+// locations are defined by their x,y coord and hallway
+var locations = {};
+locations["A"] = {x:712, y:235, hall:"AB"};
+locations["B"] = {x:528, y:235, hall:"BC"};
+locations["C"] = {x:400, y:235, hall:"CD"};
+locations["D"] = {x:220, y:235, hall:"DC"};
+locations["G"] = {x:220, y:407, hall:"GH"};
+locations["H"] = {x:220, y:555, hall:"HG"};
+locations["J"] = {x:220, y:900, hall:"JH"};
+locations["K"] = {x:30, y:850, hall:"KJ"};
+locations["M"] = {x:712, y:900, hall:"MO"};
+locations["N"] = {x:875, y:850, hall:"NM"};
+locations["O"] = {x:712, y:555, hall:"OZ"};
+locations["Z"] = {x:465, y:555, hall:"ZO"};
+locations["Cafeteria"] = {x:465, y:645, hall:"ZH", floor:1};
+locations["Main Office"] = {x:465, y:235, hall:"BC", floor:1};
+
+
+locations["125"] = {x:712, y:215, hall:"OA"};
+locations["148"] = {x:220, y:900, hall:"JH"};
+
+
