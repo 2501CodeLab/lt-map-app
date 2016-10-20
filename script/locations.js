b304430f16f073d2b35497bdb36758a72f028f21
idougherty <idougherty@cps.edu>
2016-10-20 21:54:44 +0000

    added locations

--- script/locations.js
+++ script/locations.js
@@ -12,7 +12,7 @@ locations["D"] = {x:220, y:235, hall:"DC"};
 locations["G"] = {x:220, y:407, hall:"GH"};
 locations["H"] = {x:220, y:555, hall:"HG"};
 locations["J"] = {x:220, y:900, hall:"JH"};
-locations["K"] = {x:30, y:850, hall:"KJ"};
+locations["I"] = {x:30, y:850, hall:"IJ"};
 locations["M"] = {x:712, y:900, hall:"MO"};
 locations["N"] = {x:875, y:850, hall:"NM"};
 locations["O"] = {x:712, y:555, hall:"OZ"};
@@ -22,7 +22,53 @@ locations["Main Office"] = {x:465, y:235, hall:"BC", floor:1};
 locations["Auditorium Foyer"] = {x:820, y:35, hall: "AA", floor:1};
 
 
+
+//odds:
 locations["125"] = {x:712, y:495, hall:"OA"};
+locations["161"] = {x:712, y:890, hall:"MN"};
+locations["149"] = {x:712, y:823, hall:"MO"};
+locations["161"] = {x:712, y:900, hall:"MO"};
+locations["147"] = {x:712, y:760, hall:"MO"};
+locations["143"] = {x:712, y:690, hall:"OM"};
+locations["145"] = {x:712, y:712, hall:"OM"};
+locations["141"] = {x:712, y:625, hall:"MO"};/*
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+locations[""] = {x:, y:, hall:""};
+*/
+
+//evens:
 locations["148"] = {x:220, y:900, hall:"JH"};
+locations["146"] = {x:220, y:824, hall:"JH"};
+locations["144"] = {x:220, y:760, hall:"JH"};
+locations["140"] = {x:220, y:690, hall:"HJ"};
+locations["138"] = {x:220, y:725, hall:"HJ"};
+locations["136"] = {x:220, y:625, hall:"JH"};
 
+locations["150"] = {x:100, y:850, hall:"IJ"};
+locations["154"] = {x:115, y:850, hall:"IJ"};
 
+locations["134"] = {x:270, y:550, hall:"HZ"};
+locations["132"] = {x:360, y:550, hall:"ZH"};
+locations["130"] = {x:420, y:550, hall:"ZH"};
+locations["128"] = {x:415, y:550, hall:"ZH"};
+locations["124"] = {x:350, y:550, hall:"ZH"};
+locations["122"] = {x:295, y:550, hall:"HZ"};
+
+locations["120"] = {x:220, y:460, hall:"HD"};
+locations["114"] = {x:220, y:360, hall:"DH"};
+locations["114a"] = {x:220, y:310, hall:"DH"};
+
+locations["112"] = {x:265, y:235, hall:"DC"};
+locations["110"] = {x:300, y:235, hall:"DC"};
+locations["104"] = {x:325, y:235, hall:"CD"};
+locations["106"] = {x:360, y:235, hall:"CD"};
